0cdcef4c85ba4d768b985fce7de6d25991a8be5b
Natty <86852975+nonamoes@users.noreply.github.com>
2024-07-24 12:10:48 -0600

    ajustes y hrs

--- js/main.js
+++ js/main.js
@@ -303,7 +303,7 @@
 
 	function makeTimer() {
 
-		var endTime = new Date("16 August 2024 15:00:00 GMT-06:00");
+		var endTime = new Date("16 August 2024 14:00:00 GMT-06:00");
 		endTime = (Date.parse(endTime) / 1000);
 
 		var now = new Date();
